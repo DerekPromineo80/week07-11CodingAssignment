28b02128d01b2b8d1e25d191e9d107d57b2e9a77
Derek Mcguire <derekrmc@gmail.com>
2024-09-20 19:10:23 -0400

    Week 11 MySQL Final Coding Project Update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Hello! Welcome to the Week 7 Through 11 GitHub Repository.
 
-For the Week 10 Coding Assignment:
+For the Week 11 Coding Assignment, the MySQL Final Coding Project:
 
 Please see the following Link:
 https://github.com/DerekPromineo80/week07-11CodingAssignment/tree/main/src/main/java/projects
--- src/main/resources/projects-schema.sql
+++ src/main/resources/projects-schema.sql
@@ -50,3 +50,30 @@ CREATE TABLE project_category(
 	FOREIGN KEY (category_id) REFERENCES category (category_id) ON DELETE CASCADE,
 	UNIQUE KEY (project_id, category_id)
 );
+
+-- Added for Week 11:
+-- Hang Light = project ID 9
+INSERT INTO project (project_id, project_name, estimated_hours, actual_hours, difficulty, notes) VALUES (9, 'Hang Light', 8, 7, 3, 'Get Light From Home Depot');
+
+-- Hang Light = project_id 9 - Week 11
+INSERT INTO material (project_id, material_name, num_required)
+VALUES (9, 'LightBulb', 2);
+
+INSERT INTO material (project_id, material_name, num_required)
+VALUES (9, 'Wire', 1);
+
+INSERT INTO material (project_id, material_name, num_required)
+VALUES (9, 'Light Screws', 30);
+
+-- Hang Light under Repairs for now:
+INSERT INTO project_category (project_id, category_id)
+VALUES (9, 2);
+
+INSERT INTO step (project_id, step_text, step_order)
+VALUES (9, 'Take old light down', 1);
+
+INSERT INTO step (project_id, step_text, step_order)
+VALUES (9, 'Clean the area around the old light', 2);
+
+INSERT INTO step (project_id, step_text, step_order)
+VALUES (9, 'Put new light up', 3);
